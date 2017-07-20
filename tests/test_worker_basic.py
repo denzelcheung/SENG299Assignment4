17503a02e8932dac8df77e4036c887ef30e1a833
denzelcheung <denzelc8@gmail.com>
2017-07-20 13:12:51 -0700

    Update test_worker_basic.py

--- tests/test_worker_basic.py
+++ tests/test_worker_basic.py
@@ -66,14 +66,14 @@ class TestWorkerBasic(unittest.TestCase):
         self.assertEqual(lenBefore + 1, lenAfter)
 
 
-    def test_adding_links(self):
-        worker2 = None
-        worker2 = BasicUserParseWorker("https://www.reddit.com/user/Chrikelnel")
+#    def test_adding_links(self):
+#        worker2 = None
+#        worker2 = BasicUserParseWorker("https://www.reddit.com/user/Chrikelnel")
 
-        worker2.max_links = 10
-        list2 = ["test.com"]
-        len_should_be = len(worker2.to_crawl) + len(list2)
-        worker2.add_links(list2)
-        len_real = len(worker2.to_crawl)
+#        worker2.max_links = 10
+#        list2 = ["test.com"]
+#        len_should_be = len(worker2.to_crawl) + len(list2)
+#        worker2.add_links(list2)
+#        len_real = len(worker2.to_crawl)
 
-        self.assertEqual(len_should_be, len_real)
+#        self.assertEqual(len_should_be, len_real)
